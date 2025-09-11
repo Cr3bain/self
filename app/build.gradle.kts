60a25f5c1ebd795126bf83e1bbad1515f7d881b3
Ş.Ogun Gundogdu <ogun.gundogdu@gmail.com>
2025-09-11 17:18:38 +0300

    Version updated...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "tr.com.gndg.self"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.1"
+        versionCode = 11
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
